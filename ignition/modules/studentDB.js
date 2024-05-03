52b0f9d49a40081a44b8d0cec8247cfd404260e4
Bhumika41 <bbhumi331@gmail.com>
2024-05-03 11:31:10 +0530

    first

--- ignition/modules/studentDB.js
+++ ignition/modules/studentDB.js
@@ -2,5 +2,5 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 module.exports=buildModule("StudentRecord",(m)=>{
     const student=m.contract("StudentRecord");
 
-    return[student];
+    return{student};
 })
